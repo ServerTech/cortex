cf8e9e7b9983e519b4e2f316f6bd0ffefbefeff1
Shreyas Vinod <layer@shreyasvinod.me>
2015-11-18 03:06:40 +0100

    Added move generation for pawns

--- src/cortex.cpp
+++ src/cortex.cpp
@@ -3,12 +3,15 @@
 #include "defs.h"
 #include "board.h"
 #include "move.h"
+#include "movegen.h"
 #include "hash.h"
 
 #define FEN_START "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define FEN_1 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
 #define FEN_2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
 #define FEN_3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+#define PAWN_TEST_WHITE "4kq2/2P1P3/8/3pPb1r/p2n2P1/2P4p/P1P4P/4K3 w - d6 0 1"
+#define PAWN_TEST_BLACK "4Kq2/2p1p3/8/3PpB1R/P2N2p1/2p4P/p1p4p/4k3 b - d6 0 1"
 
 int main()
 {
@@ -16,6 +19,11 @@ int main()
 
     Board board;
 
-    parse_fen(board, FEN_2);
-    std::cout << pretty_board(board) << std::endl;
+    if(!parse_fen(board, PAWN_TEST_BLACK)) std::cout << "Parse error." << std::endl;
+    else std::cout << pretty_board(board) << std::endl;
+
+    std::cout << std::endl;
+
+    MoveList ml = gen_moves(board);
+    std::cout << pretty_move_list(ml.list) << std::endl;
 }
--- src/movegen.cpp
+++ src/movegen.cpp
@@ -40,8 +40,10 @@ std::string pretty_move_list(const std::vector<Move>& list)
 
     for(int i = 0; i < s; i++)
     {
-        pretty_str << "Move " << i << ": " << COORD_MOVE(list.at(i).move);
-        pretty_str << "    Score: " << list.at(i).score << "\n";
+        pretty_str << "Move " << i + 1 << ": " << COORD_MOVE(list.at(i).move);
+        pretty_str << "    Score: " << list.at(i).score;
+        pretty_str << "    Captured: " << CAPTURED(list.at(i).move);
+        pretty_str << "    Promoted: " << PROMOTED(list.at(i).move) << "\n";
     }
 
     pretty_str << "\nTotal moves: " << s;
@@ -59,7 +61,7 @@ std::string pretty_move_list(const std::vector<Move>& list)
     @return void.
 */
 
-void push_quiet_move(std::vector<Move>& list, unsigned int move,
+inline void push_quiet_move(std::vector<Move>& list, unsigned int move,
     const Board& board)
 {
     Move move_to_add(move, 0);
@@ -76,24 +78,7 @@ void push_quiet_move(std::vector<Move>& list, unsigned int move,
     @return void.
 */
 
-void push_capture_move(std::vector<Move>& list, unsigned int move,
-    const Board& board)
-{
-    Move move_to_add(move, 0);
-    list.push_back(move_to_add);
-}
-
-/**
-    @brief Pushes an en passant capture move to the move list vector.
-
-    @param list is the move list vector.
-    @param move is an integer value representing the move.
-    @param board is the board the move is being made on.
-
-    @return void.
-*/
-
-void push_en_pas_cap_move(std::vector<Move>& list, unsigned int move,
+inline void push_capture_move(std::vector<Move>& list, unsigned int move,
     const Board& board)
 {
     Move move_to_add(move, 0);
@@ -115,7 +100,272 @@ MoveList gen_moves(const Board& board)
     MoveList ml;
     ml.list.reserve(50); // Reserve enough memory for fifty moves.
 
-    
+    const uint64 white_bb = board.chessboard[ALL_WHITE]; // White bitboard.
+    const uint64 black_bb = board.chessboard[ALL_BLACK]; // Black bitboard.
+
+    const uint64 OCC = white_bb | black_bb; // Occupied bitboard.
+
+    const uint64 FREE = ~OCC; // Free bitboard.
+
+    uint64 moves; // Temporary bitboard to store moves as they are generated.
+    unsigned int uint_1, uint_2, uint_3; // Temporary variables.
+    uint64 u64_1, u64_2, u64_3, u64_4; // Temporary variables.
+    unsigned int bit_cnt; // Number of bits in 'moves'; temporary variable.
+
+    // Pawns
+
+    // White Pawns
+
+    if(board.side == WHITE)
+    {
+        u64_1 = board.chessboard[wP];
+        bit_cnt = CNT_BITS(u64_1);
+
+        for(unsigned int i = 0; i < bit_cnt; i++)
+        {
+            // One cell upward
+
+            uint_1 = POP_BIT(u64_1);
+            u64_2 = GET_BB(uint_1);
+            u64_3 = 0ULL;
+            u64_3 |= (u64_2 << 8) & FREE;
+
+            if(u64_3 != 0ULL)
+            {
+                u64_4 = u64_3;
+                uint_2 = POP_BIT(u64_4);
+
+                if(u64_3 & B_RANK[8]) // Check if the pawn reached rank 8.
+                {
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, wB, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, wR, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, wN, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, wQ, 0), board);
+                }
+                else
+                {
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, EMPTY, 0),
+                        board);
+                }
+
+                // Two cells upward
+
+                u64_2 = (u64_2 << 16) & B_RANK[2] & FREE;
+
+                if(u64_2 != 0ULL) push_quiet_move(ml.list,
+                    GET_MOVE(uint_1, POP_BIT(u64_2), EMPTY, EMPTY, MFLAGPS),
+                    board);
+            }
+
+            // Captures
+
+            u64_2 = GET_BB(uint_1);
+
+            // Capture left
+
+            u64_3 = (u64_2 << 7) & B_RANK[GET_RANK(uint_1 + 8)] &
+               (black_bb | GET_BB(board.en_pas_sq));
+
+            if(u64_3 != 0)
+            {
+                uint_2 = determine_type(board, u64_3);
+                u64_4 = u64_3;
+                uint_3 = POP_BIT(u64_4);
+
+                ml.attacked |= u64_3;
+
+                if(uint_3 == board.en_pas_sq)
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, bP, EMPTY, MFLAGEP),
+                        board);
+                }
+                else if(u64_3 & B_RANK[8]) // Check if the pawn reached rank 8.
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wB, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wR, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wN, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wQ, 0), board);
+                }
+                else
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, EMPTY, 0), board);
+                }
+            }
+
+            // Capture right
+
+            u64_3 = (u64_2 << 9) & B_RANK[GET_RANK(uint_1 + 8)] &
+                (black_bb | GET_BB(board.en_pas_sq));
+
+            if(u64_3 != 0)
+            {
+                uint_2 = determine_type(board, u64_3);
+                u64_4 = u64_3;
+                uint_3 = POP_BIT(u64_4);
+
+                ml.attacked |= u64_3;
+
+                if(uint_3 == board.en_pas_sq)
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, bP, EMPTY, MFLAGEP),
+                        board);
+                }
+                else if(u64_3 & B_RANK[8]) // Check if the pawn reached rank 8.
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wB, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wR, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wN, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, wQ, 0), board);
+                }
+                else
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, EMPTY, 0), board);
+                }
+            }
+        }
+    }
+    else // Black Pawns
+    {
+        u64_1 = board.chessboard[bP];
+        bit_cnt = CNT_BITS(u64_1);
+
+        for(unsigned int i = 0; i < bit_cnt; i++)
+        {
+            // One cell downward
+
+            uint_1 = POP_BIT(u64_1);
+            u64_2 = GET_BB(uint_1);
+            u64_3 = 0ULL;
+            u64_3 |= (u64_2 >> 8) & FREE;
+
+            if(u64_3 != 0ULL)
+            {
+                u64_4 = u64_3;
+                uint_2 = POP_BIT(u64_4);
+
+                if(u64_3 & B_RANK[1]) // Check if the pawn reached rank 1.
+                {
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, bB, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, bR, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, bN, 0), board);
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, bQ, 0), board);
+                }
+                else
+                {
+                    push_quiet_move(ml.list,
+                        GET_MOVE(uint_1, uint_2, EMPTY, EMPTY, 0),
+                        board);
+                }
+
+                // Two cells downward
+
+                u64_2 = (u64_2 >> 16) & B_RANK[6] & FREE;
+
+                if(u64_2 != 0ULL) push_quiet_move(ml.list,
+                    GET_MOVE(uint_1, POP_BIT(u64_2), EMPTY, EMPTY, MFLAGPS),
+                    board);
+            }
+
+            // Captures
+
+            u64_2 = GET_BB(uint_1);
+
+            // Capture left
+
+            u64_3 = (u64_2 >> 7) & B_RANK[GET_RANK(uint_1 - 8)] &
+               (white_bb | GET_BB(board.en_pas_sq));
+
+            if(u64_3 != 0)
+            {
+                uint_2 = determine_type(board, u64_3);
+                u64_4 = u64_3;
+                uint_3 = POP_BIT(u64_4);
+
+                ml.attacked |= u64_3;
+
+                if(uint_3 == board.en_pas_sq)
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, wP, EMPTY, MFLAGEP),
+                        board);
+                }
+                else if(u64_3 & B_RANK[1]) // Check if the pawn reached rank 1.
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bB, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bR, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bN, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bQ, 0), board);
+                }
+                else
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, EMPTY, 0), board);
+                }
+            }
+
+            // Capture right
+
+            u64_3 = (u64_2 >> 9) & B_RANK[GET_RANK(uint_1 - 8)] &
+                (white_bb | GET_BB(board.en_pas_sq));
+
+            if(u64_3 != 0)
+            {
+                uint_2 = determine_type(board, u64_3);
+                u64_4 = u64_3;
+                uint_3 = POP_BIT(u64_4);
+
+                ml.attacked |= u64_3;
+
+                if(uint_3 == board.en_pas_sq)
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, wP, EMPTY, MFLAGEP),
+                        board);
+                }
+                else if(u64_3 & B_RANK[1]) // Check if the pawn reached rank 1.
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bB, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bR, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bN, 0), board);
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, bQ, 0), board);
+                }
+                else
+                {
+                    push_capture_move(ml.list,
+                        GET_MOVE(uint_1, uint_3, uint_2, EMPTY, 0), board);
+                }
+            }
+        }
+    }
 
     return ml;
 }
